b0753568d5af8eefd84403d4c399d4853e1ee509
Lokesh-508 <lokeshsunkari508@gmail.com>
2023-06-09 19:24:15 +0530

    first ccommit

---
+++ src/Components/ap.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const DataComponent = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://104.211.219.98/train/register');
+        setData(response.data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default DataComponent;
